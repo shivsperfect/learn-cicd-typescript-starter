12914a2f177a243f327848695de59307dd40e64d
Sivakumar <siva.kumar@bigsteptech.com>
2025-12-22 11:25:17 +0530

    remove unused function from main.ts

--- src/main.ts
+++ src/main.ts
@@ -29,11 +29,6 @@ app.use(
   }),
 );
 
-function unused() {
-  // this function does nothing
-  // and is called nowhere
-}
-
 app.use("/", express.static(path.join(__dirname, config.api.filepathRoot)));
 
 const v1Router = express.Router();
